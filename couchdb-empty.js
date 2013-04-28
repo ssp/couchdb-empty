92f9a4cfbf4ef63a57b0e8bef7710608a457de1b
Sven-S. Porst <ssp-web@earthlingsoft.net>
2013-04-28 18:09:30 +0200

    allow overriding the DB URL

--- couchdb-empty.js
+++ couchdb-empty.js
@@ -5,38 +5,50 @@
  *
  * requires node.js with nano
  *
- * Invoke like:
+ * Invoke as:
  * couchdb-empty.js database1 database2 …
  *
  * 2013 Sven-S. Porst <ssp-web@earthlingsoft.net>
  */
-var couch = require('nano')('http://localhost:5984');
 
-process.argv.forEach(function(value, index, array) {
-	if (index > 1) {
-		var db = couch.use(value);
-		db.list(function(err, body){
-			if (!err) {
-				console.log('Deleting most records from »' + value + '«');
-				var bulkDocs = [];
-				body.rows.forEach(function(value, index){
-					var id = value.id;
-					if (id[0] != '_') {
-						var rev = value.value.rev;
-						bulkDocs.push({'_id': id, '_rev': rev, '_deleted': true});
-					}
-					else {
-						console.log('Preserving »' + id + '«');
-					}
-				});
-				db.bulk({'docs':bulkDocs}, {}, function(err, body) {
-					console.log([err, body]);
-				});
-			}
-			else {
-				console.log(err);
-			}
-		});
+var couchURL = 'http://localhost:5984'
+
+var arguments = process.argv.splice(2);
+if (arguments.length >= 2) {
+	if (arguments[0] === '-couchURL') {
+		couchURL = arguments[1];
+		arguments = arguments.splice(2);
 	}
+}
+
+var couch = require('nano');
+
+arguments.forEach(function(dbName, index, array) {
+	var db = couch({'url':couchURL, 'db':dbName});
+	db.list(function(err, body){
+		if (!err) {
+			console.log('Using CouchDB »' + dbName + '« at »' + couchURL + '«');
+			var bulkDocs = [];
+			body.rows.forEach(function(value, index){
+				var id = value.id;
+				if (id[0] != '_') {
+					var rev = value.value.rev;
+					bulkDocs.push({'_id': id, '_rev': rev, '_deleted': true});
+				}
+				else {
+					console.log(dbName + ': Preserving »' + id + '«');
+				}
+			});
+			console.log(dbName + ': Deleting ' + bulkDocs.length + ' documents');
+			db.bulk({'docs':bulkDocs}, {}, function(err, body) {
+				if (err) {
+					console.log(dbName + ': bulk load problem: ' + err);
+				}
+			});
+		}
+		else {
+			console.log(dbName + ': list problem: ' + err + body);
+		}
+	});
 });
 
